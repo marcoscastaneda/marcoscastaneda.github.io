de11e31d47c58a6b6f6d7b8630540cb0f9dccb39
Marcos Castaneda <marcosjcastaneda@gmail.com>
2017-01-28 00:20:06 -0500

    Added mobile-friendly footer and CSS, added image alt tags, added hover effect for images

--- src/components/experience/ExperienceItem.js
+++ src/components/experience/ExperienceItem.js
@@ -11,14 +11,15 @@ const ProjectItem = ({
     <article className="media">
       <figure className="media-left">
         <p className="image is-64x64">
-          <img src={`./dist/img/${img}`} />
+          <img src={`./dist/img/${img}`} alt={company} title={company} />
         </p>
       </figure>
       <div className="media-content">
         <div className="content">
           <p>
             <strong>{title}</strong> at <span>{company}</span>
-            <span className="is-pulled-right has-text-right">{date}</span>
+            <span className="is-pulled-right has-text-right is-hidden-mobile">{date}</span>
+            <span className="is-hidden-tablet"><br />{date}</span>
             <br />
             {description}
           </p>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,19 +6,26 @@ const Footer = ({
     url,
   }) => (
     <footer className="footer">
-      <div className="container level">
-        <div className="level-left">
-          <p className="control">
-            <a className="button" href={url} title="View Source" target="_blank">
-              <span className="icon">
-                <i className="fa fa-github" />
-              </span>
-              <span>View Source</span>
+      <div className="container">
+        <div className="columns">
+          <div className="column is-hidden-mobile">
+            <p className="control">
+              <a className="button" href={url} title="View Source" target="_blank">
+                <span className="icon">
+                  <i className="fa fa-github" />
+                </span>
+                <span>View Source (v{version})</span>
+              </a>
+            </p>
+          </div>
+          <div className="column is-hidden-mobile has-text-centered">
+            <a className="icon" href="#" title="Return to top">
+              <i className="fa fa-hand-o-up" />
             </a>
-          </p>
-        </div>
-        <div className="level-right has-text-right">
-          <p>Built by Marcos Castaneda<br />v{version} | Made in Miami 🌴</p>
+          </div>
+          <div className="column has-text-right">
+            <p>Built by Marcos Castaneda<br />Made in Miami 🌴</p>
+          </div>
         </div>
       </div>
     </footer>
